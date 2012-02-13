727c8b8769c419b58bb023bd683dd095f5864c30
Martin Pärtel <martin.partel@gmail.com>
2012-02-13 10:14:05 +0200

    Refactored test to use test_unit.

--- test/test_thin_backups.rb
+++ test/test_thin_backups.rb
@@ -5,11 +5,48 @@ require 'shellwords'
 require 'fileutils'
 require 'date'
 
-script = File.expand_path(File.dirname(__FILE__) + "/../thin-backups.rb")
-
-Dir.mktmpdir do |tmpdir|
-  Dir.chdir(tmpdir) do
-    times = [
+class TestThinBackups < Test::Unit::TestCase
+  
+  def test_basic_functionality
+    in_tmpdir do
+      times = some_times
+      make_files(times)
+      
+      rules = "[[3, [1,3,5,7]], [8, []]]"
+      today = Date.new(2010, 3, 10).to_s
+      run_script(rules, today)
+      
+      expected_times = [
+        #'2010 03 01', # mon
+        #'2010 03 02', # tue
+        '2010 03 03', # wed
+        #'2010 03 04', # thu
+        '2010 03 05', # fri
+        #'2010 03 06', # sat
+        '2010 03 07', # sun
+        '2010 03 08', # mon
+        '2010 03 09', # tue
+        '2010 03 10'  # wed
+      ]
+      check_result(times, expected_times)
+    end
+  end
+  
+private
+  def in_tmpdir(&block)
+    Dir.mktmpdir do |tmpdir|
+      Dir.chdir(tmpdir) do
+        block.call
+      end
+    end
+  end
+  
+  def make_files(times)
+    times.map {|t| t.gsub(' ', '') }.each {|t| FileUtils.touch("foo-#{t}.tar.gz") }
+  end
+  
+  def some_times
+    [
       '2010 03 01', # mon
       '2010 03 02', # tue
       '2010 03 03', # wed
@@ -21,29 +58,9 @@ Dir.mktmpdir do |tmpdir|
       '2010 03 09', # tue
       '2010 03 10'  # wed
     ]
-    
-    times.map {|t| t.gsub(' ', '') }.each {|t| FileUtils.touch("foo-#{t}.tar.gz") }
-    
-    rules = "[[3, [1,3,5,7]], [8, []]]"
-    
-    now = Date.new(2010, 3, 10).to_s
-    
-    system(Shellwords.join([script, '.', rules, now]))
-    raise $?.inspect unless $?.success?
-    
-    expected_times = [
-      #'2010 03 01', # mon
-      #'2010 03 02', # tue
-      '2010 03 03', # wed
-      #'2010 03 04', # thu
-      '2010 03 05', # fri
-      #'2010 03 06', # sat
-      '2010 03 07', # sun
-      '2010 03 08', # mon
-      '2010 03 09', # tue
-      '2010 03 10'  # wed
-    ]
-    
+  end
+  
+  def check_result(times, expected_times)
     times.each do |t|
       filename = "foo-#{t.gsub(' ', '')}.tar.gz"
       if expected_times.include?(t)
@@ -52,7 +69,15 @@ Dir.mktmpdir do |tmpdir|
         raise "Remained but should have been deleted: #{filename}" if File.exist?(filename)
       end
     end
-    
-    puts "OK"
+  end
+  
+  def run_script(rules, initial_date, extra_args = ['-q'])
+    args = ['.', rules, initial_date] + extra_args
+    system(Shellwords.join([script, *args]))
+    raise $?.inspect unless $?.success?
+  end
+  
+  def script
+    File.expand_path(File.dirname(__FILE__) + "/../thin-backups.rb")
   end
 end
